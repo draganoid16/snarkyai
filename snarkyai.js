78c5dae458394b6a7d5ed30afbd4949005c13464
João Coelho <draganoid5@live.com>
2023-08-14 15:17:55 +0100

    security

--- snarkyai.js
+++ snarkyai.js
@@ -8,8 +8,13 @@ const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY;
 
 app.get('/get-api-key', (req, res) => {
+    const token = req.headers['x-api-token'];
+    if (token !== process.env.MY_APP_TOKEN) {
+        return res.status(403).json({ error: 'Forbidden' });
+    }
     res.json({ apiKey: API_KEY });
 });
+
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://0.0.0.0:${PORT}`);
